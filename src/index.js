5c2d7e3090ac3c5401e4d1d4fa0b7147fc43d3ce
dasha <dasha.danilchenko1990@gmail.com>
2022-11-09 12:06:33 +0200

    game algorithm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
+let computerChoice
+const resultText = document.querySelector('.result-text')
+
+function caseGame(choice) {
+    switch (choice) {
+        case 1: 
+        choice = 'rock'; break
+        case 2:
+            choice = 'paper'; break 
+        case 3:
+            choice = 'scissors'; break
+        case 4:
+            choice = 'lizard'; break 
+        case 5:
+            choice = 'spock'; break
+    }
+    computerChoice = choice
+    console.log(computerChoice)
+}
 
 function randomChoice() {
-   let numRandom = Math.ceil(Math.random()*5)
-   console.log(numRandom)
+   let randomNum = Math.ceil(Math.random()*5)
+   caseGame(randomNum)
+}
+
+function determineWinner(my, computer) {
+    switch (my | computer) {
+        case 'rock'| 'scissors' || 'lizard': 
+            resultText.innerHTML = 'Won!'; break
+        case 'paper'| 'rock' || 'spock': 
+            resultText.innerHTML = 'Won!'; break
+        case 'scissors'| 'paper' || 'lizard': 
+            resultText.innerHTML = 'Won!'; break
+        case 'lizard'| 'spock' || 'paper':
+            resultText.innerHTML = 'Won!'; break
+        case 'spock'| 'scissors' || 'rock':    
+            resultText.innerHTML = 'Won!'; break
+
+    }
+}
+
+function comparison(myChoice) {
+    if (myChoice !== computerChoice) {
+        determineWinner(myChoice, computerChoice)
+    }if (myChoice === computerChoice){
+        resultText.innerHTML = 'Draw!' 
+    }
 }
 
 function choice() {
-    console.log(this.id)
     randomChoice()
+    comparison(this.id)
 }
 
 const rock = document.querySelector('#rock')
