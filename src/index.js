c2f68ca6e2ad8812bc912d300c033980f61de309
dasha <dasha.danilchenko1990@gmail.com>
2022-11-11 18:21:54 +0200

    mark game

--- src/index.js
+++ src/index.js
@@ -3,20 +3,48 @@ import ConfettiGenerator from "confetti-js"
 var confettiSettings = { target: 'my-canvas' };
 var confetti = new ConfettiGenerator(confettiSettings);
 
+const gameIcon = document.querySelectorAll('.player-container .fa-solid')
+const computerIcon = document.querySelectorAll('.player-computer .fa-solid')
+
+const rockComputer = document.querySelector('#rockComp')
+const paperComputer = document.querySelector('#paperComp')
+const scissorsComputer = document.querySelector('#scissorsComp')
+const lizardComputer = document.querySelector('#lizardComp')
+const spockComputer = document.querySelector('#spockComp')
+
+const markGamer = document.querySelector('#mark-gamer')
+const markComputer = document.querySelector('#mark-computer')
+
 let computerChoice
 const resultText = document.querySelector('.result-text')
 
+function activeIcon(arrIcon, choiceIcon) {
+    arrIcon.forEach(icon => icon.classList.remove('blue'))
+    choiceIcon.classList.add('blue')
+}
+
+function countingPoints(mark) {
+    let value = Number(mark.innerHTML)
+    value++
+    mark.innerHTML = value
+}
+
 function caseGame(choice) {
     switch (choice) {
-        case 1: 
+        case 1:
+             activeIcon(computerIcon, rockComputer)
             choice = 'rock'; break
         case 2:
+            activeIcon(computerIcon, paperComputer)
             choice = 'paper'; break 
         case 3:
+            activeIcon(computerIcon, scissorsComputer)
             choice = 'scissors'; break
         case 4:
+            activeIcon(computerIcon, lizardComputer)
             choice = 'lizard'; break 
         case 5:
+            activeIcon(computerIcon, spockComputer)
             choice = 'spock'; break
     }
     computerChoice = choice
@@ -33,16 +61,17 @@ function youDraw() {
 
 function youWon() {
     resultText.innerHTML = 'Won!'
+    countingPoints(markGamer)
     confetti.render()
 }
 
 function youLoss() {
     resultText.innerHTML = 'Loss!'
+    countingPoints(markComputer)
 }
 
 function determineWinner(gamer, computer) {
     console.log(gamer, computer)
-    
     gamer ===  'rock' && computer === ('scissors' || 'lizard')  ||
     gamer === 'paper' && computer === ('rock' || 'spock') ||
     gamer === 'scissors' && computer === ('paper' || 'lizard') ||
@@ -59,6 +88,7 @@ function comparison(myChoice) {
 }
 
 function choice() {
+    activeIcon(gameIcon, this)
     randomChoice()
     comparison(this.id)
 }
@@ -74,3 +104,5 @@ lizard.addEventListener('click', choice)
 const spock = document.querySelector('#spock')
 spock.addEventListener('click', choice)
 
+const contentLoaded = document.querySelector('.fa-rotate')
+contentLoaded.addEventListener('click', ()=> document.location.reload())
